293e8b0506279398e00f46c87ee9bc3980cff897
Wekoslav Stefanovski <swekster@gmail.com>
2024-09-25 19:49:08 +0200

    Lecture 1 - a simple malformed server

---
+++ Misc/Disposing/Resource.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposing
+{
+    internal class Resource: IDisposable
+    {
+        private bool disposedValue;
+
+        public string Name { get; set; }
+        public Resource(string name)
+        {
+            Name = name;
+            Console.WriteLine($"Resource {Name} acquired");
+        }
+
+        public void Use()
+        {
+            Console.WriteLine($"Resource {Name} used");
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine($"Disposing resource {Name}");
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                Console.WriteLine($"Resource {Name} disposed for reals");
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~Resource()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Misc/Disposing/Runner.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disposing
+{
+    public static class Runner
+    {
+        private static Resource Resource { get; set; }
+
+        //public static void Run()
+        //{
+        //    using (var resource = new Resource())
+        //    {
+        //        resource.Use();
+        //    }
+        //}
+
+        public static void Initialize()
+        {
+            Resource = new Resource("default");
+        }
+
+        public static void DoSomething()
+        {
+            Resource.Use();
+            //using (var res2 = new Resource("res2"))
+            //{
+            //    using (var res3 = new Resource("res3"))
+            //    {
+            //        using (var res4 = new Resource("res4"))
+            //        {
+            //            // here we can use res2, res3, res4
+            //        }
+            //    }
+            //}
+
+            using var res2 = new Resource("res2");
+            using var res3 = new Resource("res3");
+            var res4 = new Resource("res4");
+            // here we can use res2, res3, res4
+        }
+
+        public static void Dispose()
+        {
+            Resource.Dispose();
+        }
+    }
+}
